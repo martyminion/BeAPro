e79e5068546a42edd228b17ae0f8b4815268356c
martyminion <martkimwaweru@gmail.com>
2020-06-06 21:26:15 +0300

    ft:Setting up the views for the profile to view a single profile and to update a profile

--- award/views.py
+++ award/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import ProfileForm,ProjectForm
 from .models import Project,Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -8,7 +9,13 @@ def welcome(request):
   title = "Be A Pro"
   return render(request,'welcome.html',{"title":title})
 
-
+@login_required
+def profile(request):
+  current_user = request.user
+  title = current_user + " Profile"
+  user_profile = Profile.get_user_profile(current_user)
+  context = {"title":title,"current_user":current_user,"user_profile":user_profile}
+  return render(request,'profile/single_profile.html',context)
 
 @login_required
 def new_profile(request):
@@ -36,3 +43,30 @@ def new_profile(request):
     form = ProfileForm()
   context = {"current_user":current_user,"title":title,"form":form}
   return render(request,'new_profile.html',context)
+
+@login_required
+def update_profile(request):
+  title = "Update Profile"
+  current_user = request.user
+
+  if request.method == 'POST':
+    form = ProfileForm(request.POST, request.FILES)
+    if form.is_valid():
+      image = form.cleaned_data['picture']
+      bio = form.cleaned_data['bio']
+      first_name = form.cleaned_data['first_name']
+      last_name = form.cleaned_data['last_name']
+
+      user_profile = Profile.get_user_profile(current_user)
+      user_profile.picture = image
+      user_profile.bio = bio
+      user_profile.first_name = first_name
+      user_profile.last_name = last_name
+
+      user_profile.save()
+
+      return redirect(profile)
+  else:
+    form = ProfileForm()
+  context = {"current_user":current_user,"title":title,"form":form}
+  return render(request,'update_profile.html',context)
